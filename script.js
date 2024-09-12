533f57f2fdd3a91c92db09d0910c4fb66c68b8cb
Falgun Sarode <147540719+falgunsarode23@users.noreply.github.com>
2024-09-12 23:23:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
+}
+
+function Employee(name, age, jobTitle) {
+	this.name = name;
+	this.age = age;
+	this.jobTitle = jobTitle;
+}
+
+jobGreet (){
+	console.log("Hello,My name is ${this.name}, I am ${this.age} year old , and my job title is ${this.jobTitle}")
+}
+
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    super(name, age); // Call the parent class constructor
+    this.jobTitle = jobTitle;
+  }
+
+  // Method specific to Employee for job greeting
+  jobGreet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  }
+}
+
 
 // Do not change code below this line
 window.Person = Person;
