5c59b08be85bb6ca30273d38b86c2be63ab9706a
Falgun Sarode <147540719+falgunsarode23@users.noreply.github.com>
2024-09-13 00:04:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ Employee.prototype = Object.create(Person.prototype); // Inherit from Person
 Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function() {
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 }
 
 let person = new Person("Alice", 25);
