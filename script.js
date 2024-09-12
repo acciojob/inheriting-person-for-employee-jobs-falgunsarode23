7f4be601c10b5a69d9cd865ed86a65caac742035
Falgun Sarode <147540719+falgunsarode23@users.noreply.github.com>
2024-09-12 23:28:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,23 @@
-// complete this js code
 function Person(name, age) {
 	this.name = name;
 	this.age = age;
 }
 
-greet() {
+Person.prototype.greet = function() {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-  }
 }
 
 function Employee(name, age, jobTitle) {
-	this.name = name;
-	this.age = age;
+	Person.call(this, name, age); // Call the Person constructor
 	this.jobTitle = jobTitle;
 }
 
-jobGreet (){
-	console.log("Hello,My name is ${this.name}, I am ${this.age} year old , and my job title is ${this.jobTitle}")
+Employee.prototype = Object.create(Person.prototype); // Inherit from Person
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function() {
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
 }
-
-class Employee extends Person {
-  constructor(name, age, jobTitle) {
-    super(name, age); // Call the parent class constructor
-    this.jobTitle = jobTitle;
-  }
-
-  // Method specific to Employee for job greeting
-  jobGreet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-  }
-}
-
 
 // Do not change code below this line
 window.Person = Person;
